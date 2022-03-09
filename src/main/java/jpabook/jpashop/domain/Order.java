6e38c714784db38bea3fcee16256634f5c0ed7e9
Jaemin Kim <jaeminkim90@gmail.com>
2022-03-09 17:23:37 +0900

    feat: 양방향 연관 관계를 위해 @OneToMany에 mappedBy로 주인 연결

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -2,6 +2,8 @@ package jpabook.jpashop.domain;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "ORDERS")
@@ -12,13 +14,12 @@ public class Order {
     @Column(name= "ORDER_ID")
     private Long id;
 
-//    @Column(name = "MEMBER_ID")
-//    private Long memberId;
-
     @ManyToOne
     @JoinColumn(name = "MEMBER_ID")
     private Member member;
 
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
 
     private LocalDateTime orderDate; //orderDate
 
